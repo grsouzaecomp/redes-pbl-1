9ee63433567dc809bae35cfe8b64b825b1924a6c
Gabriel Ribeiro <109034356+grsouzaecomp@users.noreply.github.com>
2024-09-18 21:34:40 -0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,65 +1,65 @@
 import socket
 
-# Função para solicitar uma reserva ao servidor
+# Função para solicitar uma reserva ao servidor:
 def request_reserve(server_host, server_port):
-  # Cria um socket TCP/IP e conecta ao servidor
+  # Cria um socket TCP/IP e conecta ao servidor:
   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
     server.connect((server_host, server_port))
 
-    # Solicita o ID do cliente e envia para o servidor
+    # Solicita o ID do cliente e envia para o servidor:
     id_client = input("Enter Your Client ID: ")
     server.sendall(id_client.encode())
 
     while True:
       try:
-        # Recebe o menu de opções do servidor e exibe para o cliente
+        # Recebe o menu de opções do servidor e exibe para o cliente:
         menu = server.recv(4096).decode()
         print(menu)
 
-        # Cliente escolhe uma opção do menu
+        # Cliente escolhe uma opção do menu:
         option = input("\nSelect an Option: ")
         server.sendall(option.encode())
 
-        # Recebe a resposta do servidor após a escolha da opção
+        # Recebe a resposta do servidor após a escolha da opção:
         answer = server.recv(4096).decode()
         print(answer)
 
-        # Se o cliente escolher a opção de fazer uma reserva
+        # Se o cliente escolher a opção de fazer uma reserva:
         if option == "1":
-          # Solicita ao cliente o número da rota e envia para o servidor
+          # Solicita ao cliente o número da rota e envia para o servidor:
           route = input("\nEnter a Number of the Route: ")
           server.sendall(route.encode())
 
-          # Recebe e exibe as informações dos segmentos disponíveis
+          # Recebe e exibe as informações dos segmentos disponíveis:
           segment_message = server.recv(4096).decode()
           print(segment_message)
           
-          # Solicita ao cliente o número do segmento e envia para o servidor
+          # Solicita ao cliente o número do segmento e envia para o servidor:
           segment = input("\nEnter a Number of the Segment: ")
           server.sendall(segment.encode())
 
-          # Recebe e exibe as informações dos assentos disponíveis
+          # Recebe e exibe as informações dos assentos disponíveis:
           seat_message = server.recv(4096).decode()
           print(seat_message)
           
-          # Solicita ao cliente o número do assento e envia para o servidor
+          # Solicita ao cliente o número do assento e envia para o servidor:
           seat = input("\nEnter a Number of the Seat: ")
           server.sendall(seat.encode())
 
-          # Recebe a confirmação da reserva temporária do servidor
+          # Recebe a confirmação da reserva temporária do servidor:
           confirmation = server.recv(4096).decode()
           print(confirmation)
         
-        # Se o cliente escolher a opção de sair
+        # Se o cliente escolher a opção de sair:
         elif option == "4":
           break
 
       except Exception as error:
-        # Tratamento de exceção para erros de conexão ou transmissão
+        # Tratamento de exceção para erros de conexão ou transmissão:
         print(f"An error occurred: {error}")
         break
 
-# Configurações do servidor (host e porta)
+# Configurações do servidor (host e porta):
 if __name__ == "__main__":
   server_host = "localhost"
   server_port = 12345
--- server.py
+++ server.py
@@ -2,18 +2,20 @@ import socket
 import time
 from threading import Thread, Lock
 
-# Define as rotas disponíveis com seus respectivos segmentos e assentos
+# Define as rotas disponíveis com seus respectivos segmentos e assentos:
 routes = {
   "1": {"name": "Belem-Curitiba", "segments": {
     "1": {"name": "Belem to Sao Luis", "seats": [1, 2, 3]},
     "2": {"name": "Sao Luis to Fortaleza", "seats": [1, 2, 3]},
     "3": {"name": "Fortaleza to Curitiba", "seats": [1, 2, 3]}
   }},
+  
   "2": {"name": "São Paulo-Rio de Janeiro", "segments": {
     "1": {"name": "São Paulo to Campinas", "seats": [1, 2, 3, 4]},
     "2": {"name": "Campinas to São José dos Campos", "seats": [1, 2, 3, 4]},
     "3": {"name": "São José dos Campos to Rio de Janeiro", "seats": [1, 2, 3, 4]}
   }},
+  
   "3": {"name": "Porto Alegre-Salvador", "segments": {
     "1": {"name": "Porto Alegre to Curitiba", "seats": [1, 2]},
     "2": {"name": "Curitiba to São Paulo", "seats": [1, 2]},
@@ -21,68 +23,68 @@ routes = {
   }},
 }
 
-# Dicionário para armazenar reservas confirmadas dos clientes
+# Dicionário para armazenar reservas confirmadas dos clientes:
 client_reserves = {}
 
-# Dicionário para armazenar reservas temporárias, antes da confirmação
+# Dicionário para armazenar reservas temporárias, antes da confirmação:
 temporary_reserves = {}
 
-# Lock para garantir segurança em acessos concorrentes aos dados compartilhados
+# Lock para garantir segurança em acessos concorrentes aos dados compartilhados:
 lock = Lock()
 
-# Função para lidar com as conexões dos clientes
+# Função para lidar com as conexões dos clientes:
 def handle_client(connection, address):
   print(f"Established Connection with ({address})")
 
-  # Recebe o ID do cliente e o registra
+  # Recebe o ID do cliente e o registra:
   id_client = connection.recv(4096).decode().strip()
   print(f"Client ({id_client}) connected from {address}")
 
-  # Garantir que o cliente tenha um local reservado na lista de reservas
+  # Garantir que o cliente tenha um local reservado na lista de reservas:
   with lock:
     if id_client not in client_reserves:
       client_reserves[id_client] = []
 
   while True:
     try:
-      # Envia o menu de opções para o cliente
+      # Envia o menu de opções para o cliente:
       menu = "\n1. Make a Reserve\n2. Confirm Reserves\n3. View my Reserves\n4. Exit"
       print(f"Sending menu to client {id_client}: {menu}")
       connection.sendall(menu.encode())
 
-      # Recebe a opção escolhida pelo cliente
+      # Recebe a opção escolhida pelo cliente:
       option = connection.recv(4096).decode().strip()
       print(f"({id_client}) selected option {option}")
 
-      # Opção 1: Fazer uma reserva
+      # Opção 1 - Fazer uma reserva:
       if option == "1":
-        # Envia ao cliente as rotas disponíveis
+        # Envia ao cliente as rotas disponíveis:
         available_routes = "\n".join([f"{id}: {route['name']}" for id, route in routes.items()])
         connection.sendall(f"\nAvailable Routes:\n{available_routes}\n".encode())
 
-        # Recebe a rota escolhida
+        # Recebe a rota escolhida:
         id_route = connection.recv(4096).decode().strip()
         if id_route in routes:
-          # Envia ao cliente os segmentos e assentos disponíveis para a rota escolhida
+          # Envia ao cliente os segmentos e assentos disponíveis para a rota escolhida:
           segments = "\n".join([f"{id}: {segment['name']} - Seats: {', '.join(map(str, segment['seats']))}" for id, segment in routes[id_route]["segments"].items()])
           connection.sendall(f"\nAvailable Segments for Route {routes[id_route]['name']}:\n{segments}\n".encode())
 
-          # Recebe o segmento escolhido
+          # Recebe o segmento escolhido:
           id_segment = connection.recv(4096).decode().strip()
           if id_segment in routes[id_route]["segments"]:
             connection.sendall(f"\nYou Have Selected Segment {routes[id_route]['segments'][id_segment]['name']}.".encode())
 
-            # Recebe o número do assento escolhido
+            # Recebe o número do assento escolhido:
             seat = connection.recv(4096).decode().strip()
 
-            # Verifica se o assento está disponível e faz uma reserva temporária
+            # Verifica se o assento está disponível e faz uma reserva temporária:
             with lock:
               if int(seat) in routes[id_route]["segments"][id_segment]["seats"]:
                 routes[id_route]["segments"][id_segment]["seats"].remove(int(seat))
                 temporary_reserves[id_client] = {"route": routes[id_route]["name"], "segment": routes[id_route]["segments"][id_segment]["name"], "seat": seat, "id_route": id_route, "id_segment": id_segment}
-                connection.sendall(f"\nTemporarily Reserved Seat: {seat} in segment {routes[id_route]['segments'][id_segment]['name']} of route {routes[id_route]['name']}. \nConfirm within 30 seconds.\n".encode())
+                connection.sendall(f"\nTemporarily Reserved Seat: {seat} in Segment {routes[id_route]['segments'][id_segment]['name']} of Route {routes[id_route]['name']}. \nConfirm within 30 seconds.\n".encode())
                 
-                # Inicia a thread para cancelar a reserva temporária após 30 segundos
+                # Inicia a thread para cancelar a reserva temporária após 30 segundos:
                 Thread(target=cancel_temporary_reserve, args=(id_client, id_route, id_segment, seat)).start()
               else:
                 connection.sendall("\n** SEAT UNAVAILABLE **\n".encode())
@@ -91,7 +93,7 @@ def handle_client(connection, address):
         else:
           connection.sendall("\n** INVALID ROUTE **\n".encode())
 
-      # Opção 2: Confirmar a reserva temporária
+      # Opção 2 - Confirmar a reserva temporária:
       elif option == "2":
         with lock:
           if id_client in temporary_reserves:
@@ -102,16 +104,16 @@ def handle_client(connection, address):
           else:
             connection.sendall("\nYou don't Have a Temporary Reserve to Confirm.\n".encode())
 
-      # Opção 3: Visualizar reservas confirmadas
+      # Opção 3 - Visualizar reservas confirmadas:
       elif option == "3":
         if client_reserves[id_client]:
-          my_reserves = "\n".join([f"Route: {reserve['route']}, Segment: {reserve['segment']}, Seat: {reserve['seat']}" for reserve in client_reserves[id_client]])
+          my_reserves = "\n".join([f"- Route: {reserve['route']}, Segment: {reserve['segment']}, Seat: {reserve['seat']}" for reserve in client_reserves[id_client]])
           connection.sendall(f"\nYour Confirmed Reserves:\n{my_reserves}\n".encode())
         else:
           connection.sendall("\nYou don't Have a Confirmed Reserve.\n".encode())
         print(f"({id_client}) Viewed Your Reserves.")
 
-      # Opção 4: Desconectar o cliente
+      # Opção 4 - Desconectar o cliente:
       elif option == "4":
         connection.sendall("\nDisconnecting...\n".encode())
         print(f"({id_client}) Has Been Disconnected.")
@@ -123,7 +125,7 @@ def handle_client(connection, address):
 
   connection.close()
 
-# Função que cancela a reserva temporária após 30 segundos se não for confirmada
+# Função que cancela a reserva temporária após 30 segundos se não for confirmada:
 def cancel_temporary_reserve(id_client, id_route, id_segment, seat):
   time.sleep(30)
   with lock:
@@ -132,23 +134,23 @@ def cancel_temporary_reserve(id_client, id_route, id_segment, seat):
       routes[id_route]["segments"][id_segment]["seats"].append(int(seat))
       print(f"Temporary Reserve from ({id_client}) expired. Seat {seat} in segment {routes[id_route]['segments'][id_segment]['name']} available.")
 
-# Função que inicializa o servidor e aceita conexões de clientes
+# Função que inicializa o servidor e aceita conexões de clientes:
 def start_server():
   host = 'localhost'
   port = 12345
 
-  # Configuração do socket do servidor
+  # Configuração do socket do servidor:
   server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   server_socket.bind((host, port))
   server_socket.listen()
 
   print("\nServer Started...\n")
 
-  # Aceita conexões de clientes e cria uma nova thread para cada cliente
+  # Aceita conexões de clientes e cria uma nova thread para cada cliente:
   while True:
     connection, address = server_socket.accept()
     Thread(target=handle_client, args=(connection, address)).start()
 
-# Início da execução do servidor
+# Início da execução do servidor:
 if __name__ == "__main__":
   start_server()
